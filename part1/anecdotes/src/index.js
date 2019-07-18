3290fc8e37594cb4272084a3449e2cd47fa3d33f
ecolon1 <ecolon@oriontek.do>
2019-07-17 23:22:23 -0400

    anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({handleClick,text}) => {
+const Button = ({ handleClick, text }) => {
+    return (
+        <>
+            <button onClick={handleClick()}>{text}</button>
+        </>
+    )
+}
+
+const Title = ({ title }) => {
     return (
         <div>
-            <button onClick={handleClick()}>{text}</button>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+const Anecdote = ({ text }) => {
+    return (
+        <div>
+            <p>{text}</p>
+        </div>
+    )
+}
+
+const Total = ({ total }) => {
+    return (
+        <div>
+            <span>has {total} votes</span>
         </div>
     )
 }
@@ -13,35 +37,43 @@ const App = (props) => {
     const [selected, setSelected] = useState(0)
     const [vote, setVote] = useState(0);
     const [points, setPoints] = useState([])
-    let random = 0
-    
+    const [mostVotedPosition, setMostVotedPosition] = useState(0)
+
+
     const nextAnecdote = () => () => {
-        random = getRandom()
-    setVote(random)
-    setSelected(random)
+        const random = getRandom()
+        setVote(random)
+        setSelected(random)
     }
 
     const handleVote = () => () => {
         const copy = [...points]
-        if(copy[vote]=== undefined){
+        if (copy[vote] === undefined) {
             copy[vote] = 0
         }
-         copy[vote] += 1
+        copy[vote] += 1
         setPoints(copy)
-        console.log(points)
-        
+
+        if (copy[vote] > copy[mostVotedPosition]) {
+            setMostVotedPosition(vote)
+        }
+
     }
 
     return (
         <div>
-            {props.anecdotes[selected]}
-            <br></br>
-            <span>has {points[vote] === undefined ? 0 : points[vote]} votes</span>
-            <br></br>
+            <Title title="Anecdote of the day"></Title>
+            <Anecdote text={props.anecdotes[selected]}></Anecdote>
+            <Total total={points[vote] === undefined ? 0 : points[vote]}></Total>
+
             <Button handleClick={handleVote} text="vote"></Button>
             <Button handleClick={nextAnecdote} text="next anecdote"></Button>
+            <br></br>
+            <Title title="Anecdote with most votes"></Title>
+            <Anecdote text={props.anecdotes[mostVotedPosition]}></Anecdote>
+            <Total total={points[mostVotedPosition] === undefined ? 0 : points[mostVotedPosition]}></Total>
+
         </div>)
-    
 }
 
 const anecdotes = [
@@ -53,9 +85,9 @@ const anecdotes = [
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-const min=0 
-const max=anecdotes.length  
-const getRandom =  () => Math.floor(Math.random() * (+max - +min)) + +min
+const min = 0
+const max = anecdotes.length
+const getRandom = () => Math.floor(Math.random() * (+max - +min)) + +min
 
 ReactDOM.render(
     <App anecdotes={anecdotes} />,
