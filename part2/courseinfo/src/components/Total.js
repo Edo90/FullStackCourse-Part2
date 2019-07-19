0bf8b69d8cc66366c727f2bd90bfbc7c754b52d8
ecolon1 <ecolon@oriontek.do>
2019-07-19 15:38:41 -0400

    course contents step6

---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Total = (props) => {
+    return (
+        <>
+            {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
+        </>
+    )
+}
+
+export default Total
