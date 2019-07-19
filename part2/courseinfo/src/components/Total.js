f3edc3d0c34c86a3599eecb8a5d8fc7ca0cb39c2
ecolon1 <ecolon@oriontek.do>
2019-07-19 15:44:48 -0400

    Course contents step7

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Total = (props) => {
+
+    const total = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
+
     return (
         <>
-            {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
+            <h4><span>Total of {total} exercises</span></h4>
         </>
     )
 }
